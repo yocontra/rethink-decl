7492136250cefed9317cbd21d625092ac71a3c3a
Contra <contra@maricopa.edu>
2016-02-01 13:57:17 -0800

    minor release

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## Change Log
+
+### upcoming (2016/02/01 21:57 +00:00)
+- [f70a48b](https://github.com/contra/rethink-decl/commit/f70a48b3cb35225e87abf536894af8bd6d1e291a) add sorting (@contra)
+- [93bc1e7](https://github.com/contra/rethink-decl/commit/93bc1e78d08831afdfdbbf1f9a3e2e8ef7b6d48d) init (@contra)
--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,23 @@ var _lodash2 = _interopRequireDefault(_lodash);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var ourOptions = ['limit', 'offset', 'page', 'tail'];
+var ourOptions = ['limit', 'offset', 'page', 'tail', 'sort'];
+
+var applySort = function applySort(q, sort) {
+  if (!Array.isArray(sort)) {
+    if (typeof sort !== 'string') {
+      throw new Error('sort must be a string or array');
+    }
+    sort = sort.split(',');
+  }
+
+  var r = q._r;
+  var orderBy = sort.map(function (prop) {
+    return prop.indexOf('-') === 0 ? r.desc({ index: prop.substring(1) }) : r.asc({ index: prop });
+  });
+
+  return q.orderBy.apply(q, orderBy);
+};
 
 exports.default = function (Model, options) {
   var filter = (0, _lodash2.default)(options, ourOptions);
@@ -17,6 +33,9 @@ exports.default = function (Model, options) {
   if (options.page) offset += options.page * limit;
 
   var q = Model.filter(filter);
+  if (options.sort) {
+    q = applySort(q, options.sort);
+  }
   if (options.tail) {
     q = q.changes();
   } else {
